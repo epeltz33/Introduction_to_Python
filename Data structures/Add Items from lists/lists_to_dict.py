1a75e558fa8e91cb47f96f7b27f1a3f4dd2bdaff
Eric Peltzman <epeltz@optonline.net>
2023-05-26 22:08:21 -0400

    Finished up to Dictionaries

--- Data structures/Add Items from lists/lists_to_dict.py
+++ Data structures/Add Items from lists/lists_to_dict.py
@@ -3,7 +3,8 @@ list_of_values = [100, 200, 300]
 
 my_dict = {}
 for x in range(len(list_of_keys)):
-    # TODO: populate the dictionary with key:value pairs from the lists
+    key = list_of_keys[x]
+    value: int = list_of_values[x]
+    my_dict[key] = value
 
 print(my_dict.items())
-
--- Strings/String formatting/string_formatting.py
+++ Strings/String formatting/string_formatting.py
@@ -1,5 +1,6 @@
 name = "John"
+age = 25
 # Note: %s is inside the string, % is after the string.
 print("Hello, PyCharm! My name is %s!" % name)
 
-print("I'm ??? years old" '???')
+print(f"I'm {age} years old" )
